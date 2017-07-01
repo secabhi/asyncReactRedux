72595cb702c29828208600b9537af23278084570
secabhi <abhiatstudy@gmail.com>
2017-07-01 16:39:48 +0530

    fresh project

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 
-import Demo from '../containers/demo'
-import WeatherList from '../containers/weather_list';
-import SearchBar from '../components/resusable/searchbar'
 export default class App extends Component {
   render() {
     return (
       <div className="container">
-        <SearchBar />
-        <WeatherList />
+        HELLO
       </div>
     );
   }
